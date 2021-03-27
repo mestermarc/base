6c564aff45b8aefa762302471f8344ccb800cd32
mestermarc <mestermarc98@gmail.com>
2021-03-28 00:57:46 +0100

    2. JUnit tests fixed

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,7 +3,6 @@ package hu.bme.mit.train.sensor;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
@@ -13,19 +12,19 @@ public class TrainSensorTest {
     TrainController mockController;
     TrainSensor sensor;
     TrainUser mockUser;
-
     @Before
     public void before() {
         mockController = mock(TrainController.class);
         mockUser = mock(TrainUser.class);
         mockUser.setAlarmState(false);
+        sensor = new TrainSensorImpl(mockController,mockUser);
         sensor.overrideSpeedLimit(50);
     }
-
         @Test
         public void Set_Minus_Limit() {
             when(mockUser.getAlarmState()).thenReturn(true);
             sensor.overrideSpeedLimit(-1);
+            mockUser.getAlarmState();
             verify(mockUser,times(1)).getAlarmState();
         }
         @Test
@@ -33,22 +32,22 @@ public class TrainSensorTest {
             when(mockUser.getAlarmState()).thenReturn(false);
             sensor.overrideSpeedLimit(51);
             sensor.overrideSpeedLimit(100);
+            mockUser.getAlarmState();
             verify(mockUser,times(1)).getAlarmState();
         }
         @Test
         public void SetLimitFrom100to50() {
-
             when(mockUser.getAlarmState()).thenReturn(true);
             sensor.overrideSpeedLimit(100);
             sensor.overrideSpeedLimit(49);
+            mockUser.getAlarmState();
             verify(mockUser,times(1)).getAlarmState();
         }
-
         @Test
         public void SetLimitFrom50to600() {
             when(mockUser.getAlarmState()).thenReturn(true);
             sensor.overrideSpeedLimit(600);
+            mockUser.getAlarmState();
             verify(mockUser,times(1)).getAlarmState();
         }
-
     }
